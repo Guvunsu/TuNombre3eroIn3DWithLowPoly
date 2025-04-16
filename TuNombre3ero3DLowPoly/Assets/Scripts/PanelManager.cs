c787d399b3174c1eb8271c7e889e352d0f8ac4da
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-04-16 04:18:26 -0600

    457

--- TuNombre3ero3DLowPoly/Assets/Scripts/PanelManager.cs
+++ TuNombre3ero3DLowPoly/Assets/Scripts/PanelManager.cs
@@ -1,85 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class PanelManager : MonoBehaviour {
-
     #region Variables
-    private bool paused = false;  // Para saber si el juego está en pausa
+    private bool paused = false;
     public string menuSceneName = "Menu"; //Menu escena del Menu ;P
-    public GameObject pausePanel, panelVictoria;           // Panel de pausa que se activará y desactivará
+    public GameObject pausePanel, panelVictoria;
+
     #endregion Variables
 
     #region LocalMethods
-    private void Start() {
-        // Inicializamos los paneles (asegurándonos que el panel de victoria está desactivado)
+    void Start() {
         panelVictoria.SetActive(false);
-        pausePanel.SetActive(false);  // Inicialmente el panel de pausa está oculto
+        pausePanel.SetActive(false);
     }
     void Update() {
-        // Activar o desactivar el menú de pausa cuando el jugador presiona "P"
-        if (Input.GetKeyDown(KeyCode.P)) {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
             if (paused) {
                 ResumeGame();
-
-            } else {
-                PauseGame();
-
             }
-
         }
     }
+
     #endregion Local Methods
 
     #region Funciones para Panel Victoria
     // agreggar panel de victoria por un setactive(true); y sus botones y activacion de la escena
     // agregar un tag al jugador en un gameobject vacio el trigger para la activacion del winCondition cuando el vato lo toque
-
-
     public void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player") || other.CompareTag("Win"))
-            if (panelVictoria != null) {
-
-                panelVictoria = null;
-                panelVictoria.SetActive(true);
-                Time.timeScale = 0f;
-            }
-
+        if (other.CompareTag("Player")) {
+            panelVictoria.SetActive(true);
+            Time.timeScale = 0f;
+        }
     }
     #endregion Funciones para Panel Victoria
 
     #region Funciones para el panel de pausa
-    // Activa el menú de pausa
+    //public void ActivationPanel(InputAction.CallbackContext value) {
+    //    if (value.performed && paused) {
+    //        ResumeGame();
+    //    } else {
+    //        PauseGame();
+    //    }
+    //}
     public void PauseGame() {
         paused = true;
-        pausePanel.SetActive(true);  // Activar el panel de pausa
-        Time.timeScale = 0f;         // Detiene el tiempo del juego
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
     }
-
-    // Desactiva el menú de pausa
     public void ResumeGame() {
         paused = false;
-        pausePanel.SetActive(false);  // Desactivar el panel de pausa
-        Time.timeScale = 1f;          // Reanuda el tiempo del juego
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
     }
-    // Reinicia el nivel actual
     public void RetryGame() {
-        Time.timeScale = 1f;  // indicarle que se reanuda desde el 1er momento del juego
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-    // Carga la escena del menú principal
     public void GoToMenu() {
         Time.timeScale = 1f;
         SceneManager.LoadScene(menuSceneName);
     }
-    // Salir del juego
     public void GoToExit() {
         //// lo mismo pero para el editor, cuando estas en unity
         //UnityEditor.EditorApplication.isPlaying = false;
+
         // para el jugador en in game
         Application.Quit();
     }
--- TuNombre3ero3DLowPoly/Assets/Scripts/PlayerMovement.cs
+++ TuNombre3ero3DLowPoly/Assets/Scripts/PlayerMovement.cs
@@ -1,197 +1,355 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
-    public enum PlayerState {
+    #region ENUM
+    public enum playerFSM {
         IDLE,
         WALKING,
         JOGGING,
         JUMPING,
-        CARRYING,
+        TAKE_IT,
         HIT,
-        GROUNDED
+        ISGROUNDED,
+        NOT_GROUNDED
     }
-    #region Variables
 
-    PlayerState currentState;
-    CharacterController controller;
-    Animator animator;
+    public enum GameState {
+        PLAYING,
+        WIN,
+        LOSE
+    }
 
+    #endregion ENUM
+
+    #region VARIABLES
+    [SerializeField] protected playerFSM Player_FSM;
+    [SerializeField] protected GameState gameFSM;
+
+    [SerializeField] Rigidbody rb;
+    [SerializeField] CharacterController controller;
+    [SerializeField] Animator animator;
+
+    [SerializeField] Transform m_handTransform;
+    private Transform currentPlatform;
+
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+
+    public Vector3 lastPlatformPosition;
+    protected Vector3 direction;
     Vector3 velocity;
+
+    [SerializeField] float gravity = -9.81f;
+
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float jogSpeed = 5f;
+    [SerializeField] float accelerationSpeed = 1.1f;
 
     bool isGrounded;
     bool isGrabbing;
 
-    [SerializeField] float walkSpeed = 30f;
-    [SerializeField] float jogSpeed = 60f;
-    [SerializeField] float jumpHeight = 20f;
+    #endregion VARIABLES
 
-    [SerializeField] float speed;
-    float gravity;
+    #region PublicUnityMethods
 
-    [SerializeField] float rotationSpeed = 50f;
-    [SerializeField] Transform m_handTransform;
-
-    private Transform currentPlatform;
-    private Vector3 lastPlatformPosition;
-
-    [SerializeField] float groundDistance;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask groundMask;
-
-    #endregion Variables
-
-    #region UnityMethods
-    private void Start() {
-        animator = GetComponent<Animator>();
-        controller = GetComponent<CharacterController>();
-        gravity = -9.81f;
-        jumpHeight = 20f;
+    //void Awake() {
+    //    //rb = GetComponent<Rigidbody>();
+    //    //controller = GetComponent<CharacterController>();
+    //    //animator = GetComponent<Animator>();
+    //}
+    void Start() {
+        Player_FSM = playerFSM.IDLE;
+        gameFSM = GameState.PLAYING;
     }
 
-    void Update() {
-        MovementPlayer();
-        JumpingPlayer();
-        UpdateAnimator();
-    }
+    void FixedUpdate() {
+        CheckGround();
 
-
-    #endregion UnityMethods
-
-    #region TriggersEnters Do Important Things
-
-    void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Plataform")) {
-            currentPlatform = other.transform;
-            lastPlatformPosition = currentPlatform.position;
-        }
-        if (other.CompareTag("Ungrabe") && Input.GetKeyDown(KeyCode.E)) {
-            other.transform.SetParent(transform, false);
-            other.transform.position = m_handTransform.position;
-        }
-    }
-    void OnTriggerStay(Collider other) {
-        if (!isGrabbing && other.CompareTag("Ungrabe") && Input.GetKeyDown(KeyCode.E)) {
-            other.transform.SetParent(m_handTransform, false);//para el padre es false y true para el objeto
-            other.transform.position = m_handTransform.position;
-            other.GetComponent<Rigidbody>().isKinematic = true;
-            isGrabbing = true;
-            StartCoroutine(timer());
+        if (!isGrounded && velocity.y > -20f) {
+            velocity.y += gravity * Time.fixedDeltaTime;
+        } else if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
         }
 
-    }
-    IEnumerator timer() {
-        yield return new WaitForSeconds(0.3f);
-        isGrabbing = true;
-    }
+        Vector3 move = new Vector3(direction.x, 0f, direction.z);
+        controller.Move(move * Time.fixedDeltaTime);
 
+        controller.Move(velocity * Time.fixedDeltaTime);
 
-    void OnTriggerExit(Collider other) {
-        if (other.CompareTag("Plataform")) {
-            currentPlatform = null;
+        switch (Player_FSM) {
+            case playerFSM.WALKING:
+                Move(moveSpeed);
+                break;
+
+            case playerFSM.JOGGING:
+                Move(jogSpeed * accelerationSpeed);
+                break;
+
+            case playerFSM.JUMPING:// creoq ue esto deberiua estar ebn negativo
+                if (isGrounded) {
+                    Player_FSM = playerFSM.ISGROUNDED;
+                    animator.SetBool("IsJumping", false);
+                }
+                break;
+
+            case playerFSM.IDLE:
+                animator.SetFloat("Speed", 0f);
+                break;
         }
-        if (other.CompareTag("Ungrabe")&& Input.GetKeyDown(KeyCode.E)) {
-            Debug.Log("Intento soltarlo pero no quiero :V");
-            isGrabbing = false;
+    }
+
+    #endregion PublicUnityMethods
+
+    #region PrivateMethods
+    void Move(float speed) {
+        Vector3 move = new Vector3(direction.x, 0, direction.z).normalized;
+        if (move.magnitude >= 0.1f) {
+            transform.Translate(move * speed * Time.fixedDeltaTime, Space.World);
+            animator.SetFloat("Speed", speed);
         }
     }
-    #endregion TriggersEnters Do Important Things
 
-    #region Moving Player & animator
-
-    #region Animator
-    void UpdateAnimator() {
-
-        float Xaxis = Input.GetAxis("Horizontal");
-        float Yaxis = Input.GetAxis("Vertical");
-
-        animator.SetFloat("Xaxis", Xaxis);
-        animator.SetFloat("Yaxis", Yaxis);
-
-        // Parámetros de salto
-        animator.SetBool("IsJumping", currentState == PlayerState.JUMPING);
+    void CheckGround() {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         animator.SetBool("IsGrounded", isGrounded);
+    }
 
-        if (Input.GetKeyDown(KeyCode.H)) {
-            animator.SetTrigger("Hit");
-            currentState = PlayerState.HIT;
+    #endregion PrivateMethods
+
+    #region PublicMethods
+    public void MovePlayerDoll(InputAction.CallbackContext value) {
+        if (value.performed && gameFSM == GameState.PLAYING) {
+            Vector2 input = value.ReadValue<Vector2>();
+            direction = new Vector3(input.x, 0f, input.y);
+            Player_FSM = playerFSM.WALKING;
+        } else if (value.canceled && gameFSM == GameState.PLAYING) {
+            Player_FSM = playerFSM.IDLE;
         }
-
-        if (isGrabbing) {
-            currentState = PlayerState.CARRYING;
+    }
+    public void JoggingPLayerDoll(InputAction.CallbackContext value) {
+        if (value.performed && gameFSM == GameState.PLAYING) {
+            Player_FSM = playerFSM.JOGGING;
+        } else if (value.canceled) {
+            Player_FSM = playerFSM.IDLE;
+        }
+    }
+    public void JumpPlayerDoll(InputAction.CallbackContext value) {
+        if (value.performed && isGrounded && Player_FSM != playerFSM.JUMPING) {
+            Player_FSM = playerFSM.JUMPING;
+            velocity.y = Mathf.Sqrt(jumpForce * -200f * gravity);
+            animator.SetBool("IsJumping", true);
+            animator.SetTrigger("IsGrounded");
+        }
+    }
+    public void InteractionPlayerDoll(InputAction.CallbackContext value) {
+        if (value.performed && isGrabbing) {
             animator.SetBool("Ungrabe", true);
+            Player_FSM = playerFSM.TAKE_IT;
         } else {
             animator.SetBool("Ungrabe", false);
         }
-
-
-
     }
-    #endregion Animator
-
-    void JumpingPlayer() {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
-            Debug.Log("Si Brinco pero no lo hago ");
-            velocity.y = Mathf.Sqrt(jumpHeight * -20f * gravity);
-            currentState = PlayerState.JUMPING;
-            animator.SetTrigger("JumpStart");
-        } else if (!isGrounded) {
-            currentState = PlayerState.GROUNDED;
+    public void HitPlayerDoll(InputAction.CallbackContext value) {
+        if (value.performed) {
+            animator.SetTrigger("Hit");
+            Player_FSM = playerFSM.HIT;
         }
-        //chatgpt 
-        //actualiza la verticalidad del eje y por la gravedad 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
     }
-    void MovementPlayer() {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0) {
-            velocity.y = -2f;
-
-
-            float m_MoveX = Input.GetAxis("Horizontal");
-            float m_MoveY = Input.GetAxis("Vertical");
-            Vector3 move = new Vector3(m_MoveX, m_MoveY);
-
-            if (move.magnitude > 0.1f) {
-                float speed = Input.GetKeyDown(KeyCode.LeftShift) ? jogSpeed : walkSpeed;
-                Debug.Log("Si troto pero no lo hago ");
-                controller.Move(move * speed * Time.deltaTime);
-                Quaternion m_targetRotation = Quaternion.LookRotation(move);
-                transform.rotation = Quaternion.Slerp(transform.rotation, m_targetRotation, Time.deltaTime * rotationSpeed);
-                currentState = Input.GetKey(KeyCode.LeftShift) ? PlayerState.JOGGING : PlayerState.WALKING;
-            } else {
-                currentState = PlayerState.IDLE;
-            }
-
-            velocity.y += gravity * Time.deltaTime;
-            controller.Move(velocity * Time.deltaTime);
-
-            // Movimiento de la plataforma
-            if (currentPlatform != null) {
-                Vector3 platformMovement = currentPlatform.position - lastPlatformPosition;
-                //Vector3 rotationPos = Rotacion de la plataforma;
-                //Player.rotation = transform.rotation + rotationPos;
-                controller.Move(platformMovement); // Mueve el personaje con la plataforma
-                lastPlatformPosition = currentPlatform.position;
-            }
-            if (isGrabbing && Input.GetKeyDown(KeyCode.E)) {
-                Debug.Log("Si tomo cosas pero no lo hago ");
-                GameObject temp_otherObj = m_handTransform.GetChild(0).gameObject;
-                temp_otherObj.transform.SetParent(null);
-                temp_otherObj.GetComponent<Rigidbody>().isKinematic = false;
-                isGrabbing = false;
-            }
-        }
-
-        #endregion MovingPlayer & animator
+    #endregion PublicMethods
 
 
 
-    }
+
+
+
+
+
+    //    #region Variables
+
+    //    playerFSM playerCurrentFSM;
+    //    CharacterController controller;
+    //    Animator animator;
+
+    //    Vector3 direction;
+
+    //    bool isGrounded;
+    //    bool isGrabbing;
+
+    //    [SerializeField] float walkSpeed = 30f;
+    //    [SerializeField] float jogSpeed = 60f;
+    //    [SerializeField] float jumpHeight = 20f;
+
+    //    [SerializeField] float rotationSpeed = 50f;
+    //    [SerializeField] float speed;
+    //    [SerializeField] float gravity = -9.81f;
+
+    //    [SerializeField] Transform m_handTransform;
+    //    private Transform currentPlatform;
+    //    private Vector3 lastPlatformPosition;
+
+    //    [SerializeField] float groundDistance;
+    //    [SerializeField] Transform groundCheck;
+    //    [SerializeField] LayerMask groundMask;
+
+    //    #endregion Variables
+
+    //    #region UnityMethods
+    //    void Start() {
+    //        //hacer continuo la gravedad a fucnion del tiempo 
+    //        animator = GetComponent<Animator>();
+    //        controller = GetComponent<CharacterController>();
+    //        jumpHeight = 20f;
+    //    }
+
+    //    void FixedUpdate() {
+    //        gravity += Time.fixedDeltaTime;
+
+    //    }
+    //    void Update() {
+    //        MovementPlayer();
+    //        JumpingPlayer();
+    //        UpdateAnimator();
+    //    }
+
+
+    //    #endregion UnityMethods
+
+    //    #region TriggersEnters Do Important Things
+
+    //    void OnTriggerEnter(Collider other) {
+    //        if (other.CompareTag("Plataform")) {
+    //            currentPlatform = other.transform;
+    //            lastPlatformPosition = currentPlatform.position;
+    //        }
+    //        if (other.CompareTag("Ungrabe") && Input.GetKeyDown(KeyCode.E)) {
+    //            other.transform.SetParent(transform, false);
+    //            other.transform.position = m_handTransform.position;
+    //        }
+    //    }
+    //    void OnTriggerStay(Collider other) {
+    //        if (!isGrabbing && other.CompareTag("Ungrabe") && Input.GetKeyDown(KeyCode.E)) {
+    //            other.transform.SetParent(m_handTransform, false);//para el padre es false y true para el objeto
+    //            other.transform.position = m_handTransform.position;
+    //            other.GetComponent<Rigidbody>().isKinematic = true;
+    //            isGrabbing = true;
+    //            StartCoroutine(timer());
+    //        }
+
+    //    }
+    //    IEnumerator timer() {
+    //        yield return new WaitForSeconds(0.3f);
+    //        isGrabbing = true;
+    //    }
+
+
+    //    void OnTriggerExit(Collider other) {
+    //        if (other.CompareTag("Plataform")) {
+    //            currentPlatform = null;
+    //        }
+    //        if (other.CompareTag("Ungrabe") && Input.GetKeyDown(KeyCode.E)) {
+    //            Debug.Log("Intento soltarlo pero no quiero :V");
+    //            isGrabbing = false;
+    //        }
+    //    }
+    //    #endregion TriggersEnters Do Important Things
+
+    //    #region Moving Player & animator
+
+    //    #region Animator
+    //    void UpdateAnimator() {
+
+    //        float Xaxis = Input.GetAxis("Horizontal");
+    //        float Yaxis = Input.GetAxis("Vertical");
+
+    //        animator.SetFloat("Xaxis", Xaxis);
+    //        animator.SetFloat("Yaxis", Yaxis);
+
+    //        // Parámetros de salto
+    //        animator.SetBool("IsJumping", playerCurrentFSM == playerFSM.JUMPING);
+    //        animator.SetBool("IsGrounded", isGrounded);
+
+    //        if (Input.GetKeyDown(KeyCode.H)) {
+    //            animator.SetTrigger("Hit");
+    //            playerCurrentFSM = playerFSM.HIT;
+    //        }
+
+    //        if (isGrabbing) {
+    //            playerCurrentFSM = playerFSM.CARRYING;
+    //            animator.SetBool("Ungrabe", true);
+    //        } else {
+    //            animator.SetBool("Ungrabe", false);
+    //        }
+
+
+
+    //    }
+    //    #endregion Animator
+
+    //    void JumpingPlayer() {
+    //        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+    //            Debug.Log("Si Brinco pero no lo hago ");
+    //            direction.y = Mathf.Sqrt(jumpHeight * -20f * gravity);
+    //            playerCurrentFSM = playerFSM.JUMPING;
+    //            animator.SetTrigger("JumpStart");
+    //        } else if (!isGrounded) {
+    //            playerCurrentFSM = playerFSM.ISGROUNDED;
+    //        }
+    //        //actualiza la verticalidad del eje y por la gravedad 
+    //        direction.y -= gravity * Time.deltaTime;
+    //        controller.Move(direction * Time.deltaTime);
+    //    }
+    //    void MovementPlayer() {
+    //        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+    //        if (isGrounded && direction.y < 0) {
+    //            direction.y = -2f;
+
+
+    //            float m_MoveX = Input.GetAxis("Horizontal");
+    //            float m_MoveY = Input.GetAxis("Vertical");
+    //            Vector3 move = new Vector3(m_MoveX, m_MoveY);
+
+    //            if (move.magnitude > 0.1f) {
+    //                float speed = Input.GetKeyDown(KeyCode.LeftShift) ? jogSpeed : walkSpeed;
+    //                Debug.Log("Si troto pero no lo hago ");
+    //                controller.Move(move * speed * Time.deltaTime);
+    //                Quaternion m_targetRotation = Quaternion.LookRotation(move);
+    //                transform.rotation = Quaternion.Slerp(transform.rotation, m_targetRotation, Time.deltaTime * rotationSpeed);
+    //                playerCurrentFSM = Input.GetKey(KeyCode.LeftShift) ? playerFSM.JOGGING : playerFSM.WALKING;
+    //            } else {
+    //                playerCurrentFSM = playerFSM.IDLE;
+    //            }
+
+    //            direction.y -= gravity * Time.deltaTime;
+    //            controller.Move(direction * Time.deltaTime);
+
+    //            // Movimiento de la plataforma
+    //            if (currentPlatform != null) {
+    //                Vector3 platformMovement = currentPlatform.position - lastPlatformPosition;
+    //                //Vector3 rotationPos = Rotacion de la plataforma;
+    //                //Player.rotation = transform.rotation + rotationPos;
+    //                controller.Move(platformMovement); // Mueve el personaje con la plataforma
+    //                lastPlatformPosition = currentPlatform.position;
+    //            }
+    //            if (isGrabbing && Input.GetKeyDown(KeyCode.E)) {
+    //                Debug.Log("Si tomo cosas pero no lo hago ");
+    //                GameObject temp_otherObj = m_handTransform.GetChild(0).gameObject;
+    //                temp_otherObj.transform.SetParent(null);
+    //                temp_otherObj.GetComponent<Rigidbody>().isKinematic = false;
+    //                isGrabbing = false;
+    //            }
+    //        }
+
+    //        #endregion MovingPlayer & animator
+
+
+
+    //    }
 
 }
