d03d77a3b53dd6b7744a13cce1da23eff8817428
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-04-20 13:52:38 -0600

    256

--- TuNombre3ero3DLowPoly/Assets/Scripts/NPC.cs
+++ TuNombre3ero3DLowPoly/Assets/Scripts/NPC.cs
@@ -3,16 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NPC : MonoBehaviour
-{
-    NavMeshAgent m_agent;
-    [SerializeField] Transform m_position;
-
-    void Start() {
-        m_agent = GetComponent<NavMeshAgent>();
+public class NPC : MonoBehaviour {
+    public enum EnemyState {
+        Idle,
+        Walking
     }
 
-    void Update() {
-        m_agent.SetDestination(m_position.position);
+    [Header("MOVEMENT")]
+    public float baseSpeed = 3.5f;
+    public float speedMultiplier = 1.0f;
+
+    public Transform player;
+    private NavMeshAgent agent;
+    private Animator animator;
+    private EnemyState currentState = EnemyState.Idle;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        agent.speed = baseSpeed * speedMultiplier;
+    }
+
+    void FixedUpdate() {
+        if (player == null) return;
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance > 1.5f) {
+            currentState = EnemyState.Walking;
+            agent.SetDestination(player.position);
+        } else {
+            currentState = EnemyState.Idle;
+            agent.ResetPath();
+        }
+
+        UpdateAnimation();
+    }
+
+    void UpdateAnimation() {
+        if (animator != null) {
+            animator.SetBool("IsWalking", currentState == EnemyState.Walking);
+        }
     }
 }
